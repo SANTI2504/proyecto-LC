a13e5e93cf95289240e99cbd899fc5b3beae1bec
SANTI2504 <62526796+SANTI2504@users.noreply.github.com>
2021-03-17 22:50:07 -0500

    Initial commit

---
+++ app/js/scripts.js
@@ -0,0 +1,15 @@
+function crear_usuario(){
+	alert("Usuario creado correctamente");
+}
+
+function actualizar_usuario(){
+	alert("Usuario actualizado correctamente");
+}
+
+function eliminar_usuario(){
+	alert("Usuario eliminado Correctamente");
+}
+
+function imprimir(){
+	window.print();
+}
---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMcontentloaled' , inicio, false);
+
+function
+
+
+function datos_env(){
+	alert("sus datos han sido enviados");
+}
+
+function cancelar(){
+	window.location = 'index.html'; 
+}
